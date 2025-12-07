85fa4fac7091acfa4a59b211898175a8923002d6
Marta Bachs <martabachs@ioc.cat>
2025-12-07 16:17:43 +0100

    Afegir funcionalitat afegir nom

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 
 console.log("Moda Global init")
+
+function  afegirNom(nom) {
+	if(nom === ""){
+		console.log("No pots afegir un contacte buit");
+	} else {
+		console.log ("Contacte afegit: " + nom);
+	}
+}
+
+afegirNom("");
+afegirNom("Miriam");
